a6f893531b992acba1314ed691e44a6dc183b5a5
[austingriffin63] <[austing36@gmail.com]>
2019-10-25 11:47:11 -0500

    hahah

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "API",
+    authDomain: "angular-angry-challenge-ag.firebaseapp.com",
+    databaseURL: "https://angular-angry-challenge-ag.firebaseio.com",
+    projectId: "angular-angry-challenge-ag",
+    storageBucket: "angular-angry-challenge-ag.appspot.com",
+    messagingSenderId: "909500327331",
+    appId: "1:909500327331:web:ac4ba9ab16f045b1412616"
+  }
+};
